234f3856df0968c60c2fa1e039ff52be0b73890c
MrThree12 <58087179+MrThree12@users.noreply.github.com>
2020-03-05 18:54:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,9 +48,9 @@
 		}
 
 		//window.onload(loadarticles);
-		/*window.onload = function(){
+		window.onload = function(){
 			loadarticles();
-		}*/
+		}
 		
 		function loadarticles() {
 			var xhttp;
@@ -93,6 +93,14 @@
 						<li class="nav-item">
 							<a class="nav-link" href="aboutpage.php">O stránce</a>
 						</li>
+						<?php
+							if($_SESSION['LoggedIn'] == true){
+								echo '
+									<li class="nav-item">
+										<a class="nav-link" href="administration.php"></span>Administrace</a>
+									</li>';
+							}
+						?>
 					</ul>
 
 					<ul class="navbar-nav ml-auto">
@@ -105,11 +113,16 @@
 									<li class="nav-item">
 										<a class="nav-link" href="register.php"><span class="fa fa-sign-in"></span> Register</a>
 									</li>';
-							} else {
+							} else { 
+								echo '
+								<li class="nav-item">
+									<a class="nav-link" href="#">' . $_SESSION['Username'] . '</a>
+								</li>
+								';
 								echo '
 								<li class="nav-item">							
-									<a class="nav-link" href="logout.php"><span class="fa fa-sign-out"></span> ' . $_SESSION['Username'] . '</a>
-								</li>'; 
+									<a class="nav-link" href="logout.php"><span class="fa fa-sign-out"></span>Odhlasit</a>
+								</li>';
 							}
 						?>
 
@@ -121,8 +134,8 @@
 
 				<div class="content" id="content">
 					<?php
-						$sqlAllArticles = "SELECT * FROM articlesNEW LIMIT 1";
-					$db = mysqli_connect('innodb.endora.cz', 'zklimapsa24fancz', 'Tf9k8joy7qs', 'ropblogdatabase');
+						/*$sqlAllArticles = "SELECT * FROM articlesNEW LIMIT 1";
+						$db = mysqli_connect('innodb.endora.cz', 'takeit', 'Takeit123', 'ropblogdatabase');
 						echo mysqli_connect_error();
 						$result = mysqli_query($db, $sqlAllArticles);
 						$row = mysqli_fetch_assoc($result);
@@ -133,7 +146,22 @@
 							echo '<h2 class="articleh"> ' . $row['title'] . '</h2><p class="articlep">' . $row['text'] . '</p><label class="articlelabel">– ' . $row['author'] . ' ' . $row['time'] . '</label>';					
 						}
 						
-						echo '<script language="javascript">alert("ERR3")</script>';
+						echo '<script language="javascript">alert("ERR3")</script>';*//*
+						$db = mysqli_connect('innodb.endora.cz', 'takeit', 'Takeit123', 'ropblogdatabase');
+						if (mysqli_connect_errno()) {
+							echo "Error: " + mysqli_connect_error();
+						}
+				
+						$sqlallarticles = "SELECT * FROM articles";
+						$articleresult = mysqli_query($db, $sqlallarticles);    
+					
+						while ($rowofarticles = mysqli_fetch_assoc($articleresult)) {
+				
+							echo "<div class=article>"; //Vypis clanku
+							echo "<h3 class=h3ARTICLE>" . $rowofarticles['title'] . "</h3><br>" . 
+								 "<p class=pARTICLE>" . $rowofarticles['text'] . "</p>" . 
+								 "<label class=labelsARTICLE>–" . $rowofarticles['author'] . " " . $rowofarticles['time']. "</label>";
+						}*/
 					?>				
 				</div>
 
